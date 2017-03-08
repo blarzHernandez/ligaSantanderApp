389d5da7aab6c771a9b320658a6f53fac1e910c9
Blarz hdz <info.redpaz@gmail.com>
2017-03-08 00:39:16 -0600

    server.js file ready

--- server/server.js
+++ server/server.js
@@ -9,13 +9,14 @@ var port = 3000;
 var app = express();
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname,'dist')));
+app.use(express.static(__dirname + '/src'));
 
-app.all('/',function(req, res, next){
-  console.log('Corriendo en el path');
+app.get('/',function(req, res){
+      res.end("Getting all position");
+
 });
 
 
 app.listen(port, hostname, function(){
-console.log("Servidor inicializado");
+console.log("Servidor inicializado en http:");
 });
