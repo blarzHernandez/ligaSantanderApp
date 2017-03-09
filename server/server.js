a0656f1dcee3f75d5f7437991aa5e427bff03a25
Blarz hdz <info.redpaz@gmail.com>
2017-03-09 07:01:31 -0600

    socket.io added

--- server/server.js
+++ server/server.js
@@ -1,20 +1,43 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
 
 
 var hostname = 'localhost';
 var port = 3000;
 
-var app = express();
+
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static('src'));
 
-app.get('/',function(req, res){
-      res.json({"Getting all position":"ok"});
+app.get('/hello',function(req, res){
 
+res.status(200).send('Hello Word');
 });
+
+//Team positions
+teamPosition = [
+                {name:'Barca',points:20},
+                {name:'Madrid', points:12}
+              ];
+
+                io.on('connection',function (socket) {
+                //  setTimeout(function(){
+                  //  socket.emit('message',{message:teamPosition});
+                    console.log("emitiendo mensaje a los 5 minutos");
+
+
+                //  },1000);
+
+
+                });
+
 
 
 app.listen(port, hostname, function(){
